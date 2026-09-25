f0d2e0d0bcaa0ea906a2c63631eddadd23e82de4
ashin-krish <ashinkrish066@gmail.com>
2026-09-25 13:43:06 +0530

    feat: add student model

--- src/main/java/com/example/projectone/model/Student.java
+++ src/main/java/com/example/projectone/model/Student.java
@@ -1,4 +1,58 @@
 package com.example.projectone.model;
 
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "student")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String course;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
