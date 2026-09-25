46c18b11b6f2e4603e1a931ac1b6bf4abd6563e3
ashin-krish <ashinkrish066@gmail.com>
2026-09-25 14:24:10 +0530

    feat: added crud operations

--- src/main/java/com/example/projectone/service/StudentService.java
+++ src/main/java/com/example/projectone/service/StudentService.java
@@ -1,8 +1,64 @@
 package com.example.projectone.service;
 
 
+import com.example.projectone.model.Student;
+import com.example.projectone.repo.StudentRepository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
-@Configuration
-public class StudentService {
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService
+{
+    final private StudentRepository studentRepository;
+
+   public StudentService(StudentRepository studentRepository)
+    {
+        this.studentRepository=studentRepository;
+    }
+
+    public Student saveStudent(Student student)
+    {
+       return studentRepository.save(student);
+    }
+
+    public boolean deleteStudent(long id)
+    {
+        Optional<Student> student = studentRepository.findById(id);
+
+        if(student.isEmpty())
+        {
+            return false;
+        }
+
+        studentRepository.delete(student.get());
+        return true;
+    }
+
+    public Optional<Student> updateStudent(long id, Student student)
+    {
+        Optional<Student> existingStudent = studentRepository.findById(id);
+
+        if(existingStudent.isPresent())
+        {
+            existingStudent.get().setEmail(student.getEmail());
+            existingStudent.get().setCourse(student.getCourse());
+            studentRepository.save(existingStudent.get());
+            return existingStudent;
+        }
+
+        return Optional.empty();
+    }
+
+    public List<Student> getAllStudent()
+    {
+       List<Student> students = studentRepository.findAll();
+
+       return students;
+    }
+
+
+
 }
